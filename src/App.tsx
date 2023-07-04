5a9e37a7e4c579dabe22b56c99aac5d5aed64553
techmeat <T5Kf8pY9w9yRVLs>
2023-07-04 21:43:15 +0200

    integrate passwordless-id

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useEffect, useState } from "react";
+import { client, parsers } from "@passwordless-id/webauthn";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // https://github.com/passwordless-id/webauthn
+  // https://github.com/passwordless-id/webauthn/blob/main/demos/js/basic.js
+
+  const isClientAvailable = client.isAvailable();
+
+  const [username, setUsername] = useState<string>(
+    window.localStorage.getItem("username") || ""
+  );
+  const [isRegistered, setIsRegistered] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const challenge =
+    window.localStorage.getItem("challenge_" + username) ||
+    window.crypto.randomUUID();
+  const [authenticationData, setAuthenticationData] = useState<any>(null);
+
+  const checkIsRegistered = useCallback(async () => {
+    setIsRegistered(!!window.localStorage.getItem("credential_" + username));
+  }, [username]);
+
+  useEffect(() => {
+    if (username) {
+      checkIsRegistered();
+    }
+  }, [checkIsRegistered, username]);
+
+  const register = useCallback(async () => {
+    const res = await client.register(username, challenge, {
+      authenticatorType: "auto",
+      userVerification: "required",
+      timeout: 60000,
+      attestation: false,
+      debug: false,
+    });
+    const parsed = parsers.parseRegistration(res);
+    window.localStorage.setItem("username", username);
+    window.localStorage.setItem("credential_" + username, parsed.credential.id);
+    window.localStorage.setItem("challenge_" + username, challenge);
+    checkIsRegistered();
+  }, [challenge, checkIsRegistered, username]);
+
+  const login = useCallback(async () => {
+    const res = await client.authenticate(
+      [window.localStorage.getItem("credential_" + username) || ""],
+      challenge,
+      {
+        authenticatorType: "auto",
+        userVerification: "required",
+        timeout: 60000,
+      }
+    );
+    const parsed = parsers.parseAuthentication(res);
+    setIsAuthenticated(true);
+    setAuthenticationData(parsed);
+  }, [challenge, username]);
+
+  if (!isClientAvailable) {
+    return <div>WebAuthn is not available</div>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div style={{ textAlign: "left", width: "80vw" }}>
+      {!isAuthenticated && (
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <b>Username:</b>{" "}
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      )}
+      <p>
+        <button
+          disabled={isRegistered || !!authenticationData}
+          onClick={register}
+        >
+          Register
+        </button>
+        <span> </span>
+        <button
+          disabled={!isRegistered || !!authenticationData}
+          onClick={login}
+        >
+          Login
+        </button>
       </p>
-    </>
-  )
+      {authenticationData && (
+        <>
+          <h3>AuthenticationData</h3>
+          <pre
+            style={{
+              width: "100%",
+              overflow: "auto",
+              background: "rgba(255, 255, 255, 0.2)",
+              padding: "20px",
+            }}
+          >
+            <code>{JSON.stringify(authenticationData, null, 2)}</code>
+          </pre>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import react from "@vitejs/plugin-react";
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: Number(env.VITE_PORT),
+    },
+  });
+};
